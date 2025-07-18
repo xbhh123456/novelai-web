b441fd177bb448e2fd783d11fc2695c52d5e0638
Administrator <Administrator@>
2025-07-18 10:30:40 +0800

    refactor.assets: 更新背景图片为 WebP 格式
    - 删除了所有 PNG 格式的背景图片文件
    - 在背景图片数组中更新了文件路径为 WebP 格式

--- src/stores/modules/background.js
+++ src/stores/modules/background.js
@@ -4,19 +4,19 @@ import { ref } from 'vue'
 
 export const useBgimagesStore = defineStore('background',() =>{
   const bgImages = ref([
-    'src/assets/backgrounds/bg1.png',
-    'src/assets/backgrounds/bg2.png',
-    'src/assets/backgrounds/bg3.png',
-    'src/assets/backgrounds/bg4.png',
-    'src/assets/backgrounds/bg5.png',
-    'src/assets/backgrounds/bg6.png',
-    'src/assets/backgrounds/bg7.png',
-    'src/assets/backgrounds/bg8.png',
-    'src/assets/backgrounds/bg9.png',
-    'src/assets/backgrounds/bg10.png',
-    'src/assets/backgrounds/bg12.png',
-    'src/assets/backgrounds/bg13.png',
-    'src/assets/backgrounds/bg14.png',
+    'src/assets/backgrounds/bg1.webp',
+    'src/assets/backgrounds/bg2.webp',
+    'src/assets/backgrounds/bg3.webp',
+    'src/assets/backgrounds/bg4.webp',
+    'src/assets/backgrounds/bg5.webp',
+    'src/assets/backgrounds/bg6.webp',
+    'src/assets/backgrounds/bg7.webp',
+    'src/assets/backgrounds/bg8.webp',
+    'src/assets/backgrounds/bg9.webp',
+    'src/assets/backgrounds/bg10.webp',
+    'src/assets/backgrounds/bg12.webp',
+    'src/assets/backgrounds/bg13.webp',
+    'src/assets/backgrounds/bg14.webp',
   ])
 
 
